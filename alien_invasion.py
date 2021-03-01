40fcc20e29e3558e9351169ab346f29f524a0880
Administrator <1483554663@qq.com>
2021-03-01 14:31:34 +0800

    111

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+        """上色"""
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """开始游戏的主环"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            """每次环时都会重绘屏幕"""
+            self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
+
+            """让最近的绘制的屏幕可见"""
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
